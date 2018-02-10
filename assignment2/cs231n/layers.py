b7580c532e9c5c8793cad5f7969e916b3ef15a44
Frank <lincy1021@163.com>
2018-02-10 20:36:53 +0800

    complete part3 and modify part2

--- assignment2/cs231n/layers.py
+++ assignment2/cs231n/layers.py
@@ -316,7 +316,8 @@ def dropout_forward(x, dropout_param):
         # TODO: Implement training phase forward pass for inverted dropout.   #
         # Store the dropout mask in the mask variable.                        #
         #######################################################################
-        pass
+        mask = np.random.rand(x.shape[0], x.shape[1]) < p
+        out = mask * x / p
         #######################################################################
         #                           END OF YOUR CODE                          #
         #######################################################################
@@ -324,7 +325,7 @@ def dropout_forward(x, dropout_param):
         #######################################################################
         # TODO: Implement the test phase forward pass for inverted dropout.   #
         #######################################################################
-        pass
+        out = x
         #######################################################################
         #                            END OF YOUR CODE                         #
         #######################################################################
@@ -351,7 +352,7 @@ def dropout_backward(dout, cache):
         #######################################################################
         # TODO: Implement training phase backward pass for inverted dropout   #
         #######################################################################
-        pass
+        dx = dout * mask / dropout_param['p']
         #######################################################################
         #                          END OF YOUR CODE                           #
         #######################################################################
